653cae0f6565b7c7be6b95650890b84b6da0045f
superuser10 <huygiap601@gmail.com>
2017-02-17 21:24:38 +0700

    fullProject
    Create by Eclipse for STM32F4

---
+++ STM32/fullProject/Debug/src/main.d
@@ -0,0 +1,103 @@
+src/main.o: ../src/main.c \
+ D:/CAN_LIN/STM32/fullProject/CMSIS/device/stm32f4xx.h \
+ D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cm4.h \
+ D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cmInstr.h \
+ D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cmFunc.h \
+ D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cm4_simd.h \
+ D:/CAN_LIN/STM32/fullProject/CMSIS/device/system_stm32f4xx.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/misc.h \
+ D:/CAN_LIN/STM32/fullProject/Utilities/stm32f4_discovery.h
+
+D:/CAN_LIN/STM32/fullProject/CMSIS/device/stm32f4xx.h:
+
+D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cm4.h:
+
+D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cmInstr.h:
+
+D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cmFunc.h:
+
+D:/CAN_LIN/STM32/fullProject/CMSIS/core/core_cm4_simd.h:
+
+D:/CAN_LIN/STM32/fullProject/CMSIS/device/system_stm32f4xx.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_fsmc.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc/misc.h:
+
+D:/CAN_LIN/STM32/fullProject/Utilities/stm32f4_discovery.h:
---
+++ STM32/fullProject/Debug/src/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/main.c \
+../src/stm32f4xx_it.c \
+../src/syscalls.c \
+../src/system_stm32f4xx.c 
+
+OBJS += \
+./src/main.o \
+./src/stm32f4xx_it.o \
+./src/syscalls.o \
+./src/system_stm32f4xx.o 
+
+C_DEPS += \
+./src/main.d \
+./src/stm32f4xx_it.d \
+./src/syscalls.d \
+./src/system_stm32f4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -DSTM32F407VGTx -DSTM32F4 -DSTM32F4DISCOVERY -DSTM32 -DDEBUG -DUSE_STDPERIPH_DRIVER -DSTM32F40XX -DSTM32F40_41xxx -I"D:/CAN_LIN/STM32/fullProject/inc" -I"D:/CAN_LIN/STM32/fullProject/CMSIS/core" -I"D:/CAN_LIN/STM32/fullProject/CMSIS/device" -I"D:/CAN_LIN/STM32/fullProject/StdPeriph_Driver/inc" -I"D:/CAN_LIN/STM32/fullProject/Utilities" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
